b5a71b266e56f39a9bd7e779958e64fd6cf8d2af
Erik Reyna <erik@erikreyna.com>
2018-07-06 11:08:52 -0400

    remove footer content

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,68 +1,57 @@
-import React from 'react'
-import { Container, Card, CardTitle, CardGroup, CardBody } from 'reactstrap'
-import Helmet from 'react-helmet'
-import graphql from 'graphql'
-import { basename } from 'path'
-import Link from 'gatsby-link'
+import React from "react";
+import { Container, Card, CardTitle, CardGroup, CardBody } from "reactstrap";
+import Helmet from "react-helmet";
+import graphql from "graphql";
+import { basename } from "path";
+import Link from "gatsby-link";
 
 // find a post title by path
-const findNode = (path, data) => data.allMarkdownRemark.edges
-  .map(edge => edge.node.frontmatter)
-  .filter(r => r.path === path)
-  .pop()
+const findNode = (path, data) =>
+  data.allMarkdownRemark.edges
+    .map(edge => edge.node.frontmatter)
+    .filter(r => r.path === path)
+    .pop();
 
-export default function Template ({ data }) {
-  const { markdownRemark: post } = data
-  const related = post.frontmatter.related ? post.frontmatter.related.map(r => findNode(r.post, data)) : []
+export default function Template({ data }) {
+  const { markdownRemark: post } = data;
+  const related = post.frontmatter.related
+    ? post.frontmatter.related.map(r => findNode(r.post, data))
+    : [];
   return (
     <div>
       <Helmet title={`Blog | ${post.frontmatter.title}`}>
         {data.site.siteMetadata.disqus && (
-          <script id='dsq-count-scr' src='//gatsby-starter-blog.disqus.com/count.js' async />
+          <script
+            id="dsq-count-scr"
+            src="//gatsby-starter-blog.disqus.com/count.js"
+            async
+          />
         )}
         {data.site.siteMetadata.disqus && (
           <script>{`(function() {
           var d = document, s = d.createElement('script');
-          s.src = 'https://${data.site.siteMetadata.disqus}.disqus.com/embed.js';
+          s.src = 'https://${
+            data.site.siteMetadata.disqus
+          }.disqus.com/embed.js';
           s.setAttribute('data-timestamp', +new Date());
           (d.head || d.body).appendChild(s);
           })();`}</script>
         )}
       </Helmet>
       <Container>
-        <h1 className='display-3'>{post.frontmatter.title}</h1>
+        <h1 className="display-3">{post.frontmatter.title}</h1>
       </Container>
 
       <Container dangerouslySetInnerHTML={{ __html: post.html }} />
 
-      {post.frontmatter.attachments && (<Container><h4>Attachments</h4><CardGroup>
-        {post.frontmatter.attachments.map((attachment, i) => (
-          <Card key={i}>
-            <CardBody>
-              <CardTitle><a href={attachment.filename}>{basename(attachment.filename)}</a></CardTitle>
-            </CardBody>
-          </Card>
-        ))}
-      </CardGroup></Container>)}
-
-      {post.frontmatter.related && (<Container><h4>Related</h4><CardGroup>
-        {related.map((r, i) => (
-          <Card key={i}>
-            <CardBody>
-              <CardTitle>
-                <Link to={r.path}>{r.title}</Link>
-              </CardTitle>
-            </CardBody>
-          </Card>
-        ))}
-      </CardGroup></Container>)}
-
-      {data.site.siteMetadata.disqus && (<Container>
-        <hr />
-        <div id='disqus_thread' />
-      </Container>)}
+      {data.site.siteMetadata.disqus && (
+        <Container>
+          <hr />
+          <div id="disqus_thread" />
+        </Container>
+      )}
     </div>
-  )
+  );
 }
 
 export const pageQuery = graphql`
@@ -72,7 +61,7 @@ export const pageQuery = graphql`
         disqus
       }
     }
-    
+
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -88,10 +77,10 @@ export const pageQuery = graphql`
       }
     }
 
-    allMarkdownRemark{
-      edges{
-        node{
-          frontmatter{
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
             title
             path
           }
@@ -99,4 +88,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
